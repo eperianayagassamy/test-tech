073a8c3a61458a171777cabb77bc46e1a74aed41
Etienne Perianayagassamy <etienne.periana@gmail.com>
2026-02-03 15:20:20 +0100

    fix: renomage de classe

--- api/src/test/java/fr/shopping/cart/domain/cart/models/OfferConstraintIT.java
+++ api/src/test/java/fr/shopping/cart/domain/cart/models/OfferConstraintIT.java
@@ -9,14 +9,13 @@ import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
-import org.springframework.dao.DataIntegrityViolationException;
 
 import java.math.BigDecimal;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @DataJpaTest
-class OfferConstraintsIT {
+class OfferConstraintIT {
 
     @Autowired
     private TestEntityManager entityManager;
