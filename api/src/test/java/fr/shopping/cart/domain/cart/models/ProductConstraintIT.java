0d80e55ee3c6fec8768cacf8ffa5efd27ee3fae9
Etienne Perianayagassamy <etienne.periana@gmail.com>
2026-02-03 14:44:38 +0100

    fix: changement nom

--- api/src/test/java/fr/shopping/cart/domain/cart/models/ProductConstraintIT.java
+++ api/src/test/java/fr/shopping/cart/domain/cart/models/ProductConstraintIT.java
@@ -15,7 +15,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @DataJpaTest
-class ProductRepositoryIT {
+class ProductConstraintIT {
 
     @Autowired
     private ProductRepository productRepository;
